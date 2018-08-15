91bc794c6731efa37c8b69fb5de03347150394c0
compalmanel <paulo.albuquerque@gmail.com>
2018-08-15 16:58:55 +0100

    correct semantics for db creation and subsequent queries

--- blocks.js
+++ blocks.js
@@ -3,45 +3,31 @@ const request = require('sync-request');
 const fs = require('fs');
 const axios = require('axios');
 
-const sqlsrc = `
-CREATE TABLE IF NOT EXISTS blocks (
+const schemaSQL = [
+  `CREATE TABLE IF NOT EXISTS blocks (
     height integer PRIMARY KEY,
     generator text NOT NULL,
     fees integer NOT NULL DEFAULT 0,
     txs integer NOT NULL DEFAULT 0,
     timestamp integer NOT NULL
-);
-CREATE INDEX IF NOT EXISTS idx_generator ON blocks (generator);
-CREATE TABLE IF NOT EXISTS leases (
+);`,
+  `CREATE INDEX IF NOT EXISTS idx_generator ON blocks (generator);`,
+  `CREATE TABLE IF NOT EXISTS leases (
     leaser text,
     start integer,
     end integer,
     node text,
     amount integer,
     PRIMARY KEY (leaser, start)
-)
-`;
-
-const insert_blocks = `
-INSERT INTO blocks (height, generator, fees, timestamp)
-VALUES (?, ?, ?, ?);
-`;
+);`];
 
 const sqlins = `
 INSERT INTO blocks (height, generator, fees, timestamp)
-VALUES (1, 'add', 1), (2, 'add', 2), (3, 'add', 3);
-`;
-
-const sqlup = `
-INSERT INTO blocks (height, generator, fees)
-SELECT 1, 'add', 1
-WHERE NOT EXISTS(SELECT 1 FROM blocks WHERE height = 1');
-`;
+VALUES (1, 'add', 1), (2, 'add', 2), (3, 'add', 3);`;
 
 const lastblock = `
 SELECT MAX(height) AS top
-FROM blocks;
-`
+FROM blocks;`;
 
 const storeBlock = function (db, block) {
   // calculate fees
@@ -55,7 +41,6 @@ const storeBlock = function (db, block) {
     }
   }, 0);
   // write the blocks db record
-  console.log(block.transactions.length);
   return db.run(`INSERT OR REPLACE INTO blocks (height, generator, fees, txs, timestamp) VALUES (?, ?, ?, ?, ?);`,
     [block.height, block.generator, fees, block.transactions.length, block.timestamp],
     function (err) {
@@ -74,7 +59,6 @@ const getBlocks = function (db) {
       'Connection': 'keep-alive'
     }
   }).getBody('utf8'));
-  //  console.log(JSON.stringify(currentBlocks));
   return currentBlocks.map(block => storeBlock(db, block));
 };
 
@@ -88,26 +72,30 @@ const db = new sqlite3.Database('blocks.db', (err) => {
   console.log('Connected to the blocks database.');
 });
 
-db.run(sqlsrc, function (err) {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log(`Schema (re)created.`);
-});
+db.serialize(() => {
 
-const row = db.get(lastblock, [], (err, row) => {
-  if (err) {
-    console.error(err.message);
-    return null;
-  }
-  return row ? console.log(row.top) : console.log(`No results`);
-});
+  // (re)create schema
+  schemaSQL.map(stmt => db.run(stmt, function (err) {
+    if (err) {
+      return console.error(err.message);
+    }
+  }));
 
-getBlocks(db);
+  // populate data
+  db.parallelize(() => getBlocks(db));
+
+  const row = db.get(lastblock, [], (err, row) => {
+    if (err) {
+      console.error(err.message);
+      return null;
+    }
+    return row ? console.log(row.top) : console.log(`No results`);
+  });
+});
 
 db.close((err) => {
   if (err) {
     console.error(err.message);
   }
-  console.log('Close the database connection.');
+  console.log('Closed the database connection.');
 });
